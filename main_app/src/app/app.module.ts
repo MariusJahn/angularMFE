8f51f79f2f2b11cca3d69c53d8c9a507126dd288
Marius Jahn <Marius.jahn@5minds.de>
2023-06-20 14:06:00 +0200

    more file structure

--- main_app/src/app/app.module.ts
+++ main_app/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 import { RouterModule } from '@angular/router';
 import { APP_ROUTES } from './app.routes';
