ec73f5d80622095d7cf16033c515ee0421864093
Marius Jahn <Marius.jahn@5minds.de>
2023-06-20 08:44:09 +0200

    import subheader correctly

--- mitarbeiter/src/app/app.component.ts
+++ mitarbeiter/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SubheaderComponent } from './subheader/subheader.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
+  imports: [SubheaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
