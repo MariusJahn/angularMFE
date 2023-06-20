5db3330db926d86acf5ee3de0479a9a9b80a5f26
Marius Jahn <Marius.jahn@5minds.de>
2023-06-20 13:28:58 +0200

    add the missing mapping parts from webpack again

--- mitarbeiter/webpack.config.js
+++ mitarbeiter/webpack.config.js
@@ -1,7 +1,12 @@
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const { mf, shareAll } = require("@angular-architects/module-federation/webpack");
+const mf = require("@angular-architects/module-federation/webpack");
 const path = require("path");
+const share = mf.share;
 
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(
+  path.join(__dirname, 'tsconfig.json'),
+  [/* mapped paths to share */]);
 
 module.exports = {
   output: {
@@ -10,6 +15,11 @@ module.exports = {
   },
   optimization: {
     runtimeChunk: false
+  },
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    }
   },
   experiments: {
     outputModule: true
@@ -28,14 +38,16 @@ module.exports = {
         './Subheader': './src/app/subheader/subheader.component.ts',
       },
 
-      shared: {
-        ...shareAll({
-          singleton: true,
-          strictVersion: true,
-          requiredVersion: 'auto',
-        }),
-      },
+      shared: share({
+        "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+        "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+        "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+        "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+
+        ...sharedMappings.getDescriptors()
+      })
 
     }),
+    sharedMappings.getPlugin()
   ],
 };
